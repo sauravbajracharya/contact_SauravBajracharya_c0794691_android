7066e1b31eefe821a959120ff57bd27eda3c9fcf
Saurav Bajracharya <sauravbajracharya35@gmail.com>
2021-06-13 17:17:07 +0545

    AddContactActivity added

--- app/src/main/java/com/example/contact_sauravbajracharya_c0794691_android/AddContactActivity.java
+++ app/src/main/java/com/example/contact_sauravbajracharya_c0794691_android/AddContactActivity.java
@@ -1,4 +1,20 @@
 package com.example.contact_sauravbajracharya_c0794691_android;
 
-public class AddContactActivity {
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddContactActivity extends AppCompatActivity {
+
+
+    private EditText etFirstName, etLastName, etEmail, etNumber, etAddress;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+    }
 }
--- app/src/main/java/com/example/contact_sauravbajracharya_c0794691_android/MainActivity.java
+++ app/src/main/java/com/example/contact_sauravbajracharya_c0794691_android/MainActivity.java
@@ -2,21 +2,25 @@ package com.example.contact_sauravbajracharya_c0794691_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.contact_sauravbajracharya_c0794691_android.model.Contact;
 import com.example.contact_sauravbajracharya_c0794691_android.model.ContactViewModel;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RecyclerViewAdapter.OnContactClickListener{
 
 
     private ContactViewModel contactViewModel;
 
     private RecyclerView recyclerView;
 
+    private RecyclerViewAdapter recyclerViewAdapter;
 
+    public static final String CONTACT_ID = "contact_id";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +32,23 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.recycler_view);
 
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        contactViewModel.getAllContacts().observe(this, contacts -> {
+            //set adapter
+            recyclerViewAdapter = new RecyclerViewAdapter(contacts, this, this);
+            recyclerView.setAdapter(recyclerViewAdapter);
+        });
+
+
+    }
+
+    @Override
+    public void onContactClick(int position) {
+        Contact contact = contactViewModel.getAllContacts().getValue().get(position);
+        Intent intent = new Intent(MainActivity.this, AddContactActivity.class);
+        intent.putExtra(CONTACT_ID, contact.getId());
+        startActivity(intent);
     }
 }
