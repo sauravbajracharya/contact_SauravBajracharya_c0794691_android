9abc8ab6cdcdd0ede96defd27ad834846e678b56
Saurav Bajracharya <sauravbajracharya35@gmail.com>
2021-06-13 17:06:05 +0545

    RecyclerViewAdapter added

--- app/src/main/java/com/example/contact_sauravbajracharya_c0794691_android/MainActivity.java
+++ app/src/main/java/com/example/contact_sauravbajracharya_c0794691_android/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.contact_sauravbajracharya_c0794691_android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.contact_sauravbajracharya_c0794691_android.model.Contact;
+import com.example.contact_sauravbajracharya_c0794691_android.model.ContactViewModel;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private ContactViewModel contactViewModel;
+
+    private RecyclerView recyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        contactViewModel = new ViewModelProvider.AndroidViewModelFactory(this.getApplication())
+                .create(ContactViewModel.class);
+
+        recyclerView = findViewById(R.id.recycler_view);
+
+
     }
 }
