d90016cbf6563e98bee5af6d3df30bf524adb6be
Saurav Bajracharya <sauravbajracharya35@gmail.com>
2021-06-13 16:26:15 +0545

    ContactRoomDatabase added

---
+++ app/src/main/java/com/example/contact_sauravbajracharya_c0794691_android/util/ContactRoomDatabase.java
@@ -0,0 +1,55 @@
+package com.example.contact_sauravbajracharya_c0794691_android.util;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import com.example.contact_sauravbajracharya_c0794691_android.data.ContactDao;
+import com.example.contact_sauravbajracharya_c0794691_android.model.Contact;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {Contact.class}, version = 1, exportSchema = false)
+public abstract class ContactRoomDatabase extends RoomDatabase {
+
+    public abstract ContactDao contactDao();
+    private static volatile ContactRoomDatabase INSTANCE;
+    private static final int NUMBER_OF_THREADS = 4;
+
+    private static final String DB_NAME = "room_contact_database";
+
+    public static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    public static ContactRoomDatabase getInstance(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (ContactRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), ContactRoomDatabase.class, DB_NAME)
+                            .addCallback(sRoomDatabaseCallback)
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+
+    private static final RoomDatabase.Callback sRoomDatabaseCallback =
+            new RoomDatabase.Callback() {
+                @Override
+                public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                    super.onCreate(db);
+                    databaseWriteExecutor.execute(() -> {
+                        ContactDao contactDao = INSTANCE.contactDao();
+                        contactDao.deleteAll();
+                    });
+                }
+            };
+
+
+}
