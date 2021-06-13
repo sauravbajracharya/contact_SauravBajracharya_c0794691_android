7a9c1ecbae7804fc6c51ac852e008a2118860ab6
Saurav Bajracharya <sauravbajracharya35@gmail.com>
2021-06-13 16:19:05 +0545

    Model Contact added

---
+++ app/src/main/java/com/example/contact_sauravbajracharya_c0794691_android/model/Contact.java
@@ -0,0 +1,95 @@
+package com.example.contact_sauravbajracharya_c0794691_android.model;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "contact")
+public class Contact {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    private int id;
+
+    @ColumnInfo(name = "first_name")
+    @NonNull
+    private String firstName;
+
+    @ColumnInfo(name = "last_name")
+    @NonNull
+    private String lastName;
+
+    @ColumnInfo(name = "email")
+    private String email;
+
+    @ColumnInfo(name = "phone_number")
+    private int phoneNumber;
+
+    @ColumnInfo(name = "address")
+    private String address;
+
+
+    @Ignore
+    public Contact() {
+    }
+
+    public Contact(@NonNull String firstName, @NonNull String lastName, String email, int phoneNumber, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @NonNull
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(@NonNull String firstName) {
+        this.firstName = firstName;
+    }
+
+    @NonNull
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(@NonNull String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
