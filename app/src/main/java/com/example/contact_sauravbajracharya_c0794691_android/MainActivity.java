ce6615483716f5857caec52cf45186c81bb163fb
Saurav Bajracharya <sauravbajracharya35@gmail.com>
2021-06-13 13:25:48 +0545

    Initial commit

---
+++ app/src/main/java/com/example/contact_sauravbajracharya_c0794691_android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.contact_sauravbajracharya_c0794691_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
