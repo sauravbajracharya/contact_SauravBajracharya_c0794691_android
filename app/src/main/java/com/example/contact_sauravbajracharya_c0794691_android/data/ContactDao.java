c34ec4b0455320fc03c51818da2aca46d0cbc02d
Saurav Bajracharya <sauravbajracharya35@gmail.com>
2021-06-13 16:21:41 +0545

    ContactDao added

--- app/src/main/java/com/example/contact_sauravbajracharya_c0794691_android/data/ContactDao.java
+++ app/src/main/java/com/example/contact_sauravbajracharya_c0794691_android/data/ContactDao.java
@@ -1,4 +1,35 @@
 package com.example.contact_sauravbajracharya_c0794691_android.data;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.contact_sauravbajracharya_c0794691_android.model.Contact;
+
+import java.util.List;
+
+@Dao
 public interface ContactDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Contact contact);
+
+    @Query("DELETE FROM contact")
+    void deleteAll();
+
+    @Query("SELECT * FROM contact ORDER BY first_name ASC")
+    LiveData<List<Contact>> getAllEmployees();
+
+    @Query("SELECT * FROM contact WHERE id == id")
+    LiveData<Contact> getEmployee();
+
+    @Update
+    void update(Contact contact);
+
+    @Delete
+    void delete(Contact contact);
 }
