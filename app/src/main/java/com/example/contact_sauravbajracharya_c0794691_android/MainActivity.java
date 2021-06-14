3df814c7f275cc5f3b3de3ce57ced1ed3099cfb6
Saurav Bajracharya <sauravbajracharya35@gmail.com>
2021-06-14 07:04:35 +0545

    new activity main

--- app/src/main/java/com/example/contact_sauravbajracharya_c0794691_android/MainActivity.java
+++ app/src/main/java/com/example/contact_sauravbajracharya_c0794691_android/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewAdapt
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_new);
 
 
         contactViewModel = new ViewModelProvider.AndroidViewModelFactory(this.getApplication())
